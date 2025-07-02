8abb1bd2a6aa366faa8e800cccf3ad80d63c56bb
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-02 23:07:35 +0500

    creating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='w-full h-auto py-10 flex justify-center items-center'>
+      <div className='flex flex-col gap-10 justify-center items-center w-full h-full'>
+        <div className="flex flex-wrap"></div>
+      </div>
     </div>
   )
 }
