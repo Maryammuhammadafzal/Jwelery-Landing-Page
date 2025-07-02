74304279997db5617cf655bbd75087da341e0b8d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-02 16:25:12 +0500

    created

--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -9,6 +9,28 @@ import Image from "next/image";
 import React from "react";
 
 const TestimonialPage = () => {
+  const testimonial_data = [
+    {
+      name: "Sophia Grace",
+      testimonial:
+        '"I’ve never felt more beautiful than when wearing my necklace from Ethereal Grace Jewelry. The craftsmanship is extraordinary."',
+    },
+    {
+      name: "Sophia Grace",
+      testimonial:
+        '"I’ve never felt more beautiful than when wearing my necklace from Ethereal Grace Jewelry. The craftsmanship is extraordinary."',
+    },
+    {
+      name: "Sophia Grace",
+      testimonial:
+        '"I’ve never felt more beautiful than when wearing my necklace from Ethereal Grace Jewelry. The craftsmanship is extraordinary."',
+    },
+    {
+      name: "Sophia Grace",
+      testimonial:
+        '"I’ve never felt more beautiful than when wearing my necklace from Ethereal Grace Jewelry. The craftsmanship is extraordinary."',
+    },
+  ];
   return (
     <div className="flex w-full h-auto justify-center md:py-20 sm:py-14 py-8 items-center">
       <div className="w-[85%] flex flex-col gap-16 h-auto justify-center items-center">
@@ -19,38 +41,39 @@ const TestimonialPage = () => {
         </div>
 
         <div className="w-full h-auto p-3 flex justify-evenly gap-14 items-center">
-          <Carousel className="w-[60%] mx-auto justify-center flex items-center">
-            <CarouselContent className="w-full">
-              <CarouselItem className="w-full mx-3 bg-[#FFF1EA] flex justify-center items-center flex-col">
-                <div className="w-full flex flex-col gap-4 justify-center items-center min-h-[300px] ">
-                  <Image
-                    src="/images/quote.png"
-                    alt={`quote-icon`}
-                    width={60}
-                    height={60}
-                    className=""
-                  />
-                
-                <p className="max-w-2xl text-center text-xl leading-8">
-                  "I’ve never felt more beautiful than when wearing my necklace
-                  from Ethereal Grace Jewelry. The craftsmanship is
-                  extraordinary."
-                </p>
-                <div className="flex flex-col gap-1 justify-center items-center m-2">
-                  <h5 className="font-sub text-3xl text-secondary">Sophia Grace</h5>
-                  <Image
-                    src="/images/border-bottom.png"
-                    alt={`border-vector`}
-                    width={100}
-                    height={30}
-                    className=""
-                  />
-                </div>
-                </div>
-              </CarouselItem>
-             
+          <Carousel className="w-[80%] mx-auto justify-center flex items-center">
+            <CarouselContent className="w-full mx-auto">
+              {testimonial_data.map((item, index) => (
+                <CarouselItem className="w-full px-20 mx-auto flex justify-center items-center flex-col">
+                  <div className="w-full flex flex-col bg-[#FFF1EA] gap-4 justify-center items-center min-h-[300px] ">
+                    <Image
+                      src="/images/quote.png"
+                      alt={`quote-icon`}
+                      width={60}
+                      height={60}
+                      className=""
+                    />
+
+                    <p className="max-w-2xl text-center text-xl leading-8">
+                      {`${item.testimonial}`}
+                    </p>
+                    <div className="flex flex-col gap-1 justify-center items-center m-2">
+                      <h5 className="font-sub text-3xl text-secondary">
+                        {item.name}
+                      </h5>
+                      <Image
+                        src="/images/border-bottom.png"
+                        alt={`border-vector`}
+                        width={100}
+                        height={30}
+                        className=""
+                      />
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
             </CarouselContent>
-            <CarouselPrevious  />
+            <CarouselPrevious />
             <CarouselNext />
           </Carousel>
         </div>
--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -173,6 +173,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
 
 function CarouselPrevious({
   className,
+    variant = "outline",
   size = "icon",
   ...props
 }: React.ComponentProps<typeof Button>) {
@@ -181,9 +182,10 @@ function CarouselPrevious({
   return (
     <Button
       data-slot="carousel-previous"
+      variant={variant}
       size={size}
       className={cn(
-        "absolute size-10 w-[50px]  bg-primary text-black",
+        "absolute size-10 w-[50px]  bg-[#FFF1EA] text-black",
         orientation === "horizontal"
           ? "top-1/2 -left-12 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -213,7 +215,7 @@ function CarouselNext({
       variant={variant}
       size={size}
       className={cn(
-        "size-10 w-[50px]  bg-primary text-black",
+        "size-10 w-[50px]  bg-[#FFF1EA] text-black",
         orientation === "horizontal"
           ? "top-1/2 -right-12 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
